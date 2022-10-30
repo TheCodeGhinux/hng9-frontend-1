49a2d2ba6e771973d50374953ee6c9f24f15f8b5
giy21 <ilesanmibry@gmail.com>
2022-10-30 11:12:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './components/profile/profile'
+import Footer from './components/footer/footer'
+
+const App = () => {
+  return (
+    <>
+        <About />
+        <Footer />
+    </>
+  )
+}
+
+export default App
